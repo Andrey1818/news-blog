71d2b8673bca80fcbc811f2e5bcbbadef97f826d
Andrey1818 <86777186+Andrey1818@users.noreply.github.com>
2021-08-09 02:25:27 +0300

    Added from windows

---
+++ src/environments/private.keys.ts
@@ -0,0 +1,6 @@
+import {Environments} from "./interface";
+
+export const PrivateKeys ={
+  apiKey:"AIzaSyByOvzD4um59HU87p3mTTo25gzXEIF1iCU",
+  fbDbKey: "https://anguler-blog-default-rtdb.europe-west1.firebasedatabase.app/"
+}
