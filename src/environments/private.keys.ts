772cc0398c67fd7f3899ddbf1e4d5e39f9ce5398
Andrey1818 <86777186+Andrey1818@users.noreply.github.com>
2021-08-10 19:30:39 +0300

    Update private.keys.ts

--- src/environments/private.keys.ts
+++ src/environments/private.keys.ts
@@ -1,6 +1,1 @@
-import {Environments} from "./interface";
 
-export const PrivateKeys ={
-  apiKey:"AIzaSyByOvzD4um59HU87p3mTTo25gzXEIF1iCU",
-  fbDbKey: "https://anguler-blog-default-rtdb.europe-west1.firebasedatabase.app/"
-}
